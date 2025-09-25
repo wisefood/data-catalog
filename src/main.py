00f47a59a1161662703794db3f781cfcfa49f83a
petroud <petroudimitris@outlook.com>
2025-09-25 09:56:38 +0000

    Updated repo structure

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from .service import stores
+import uvicorn
 
 # create FastAPI app
 api = FastAPI(title="WiseFood Catalog API")
@@ -14,3 +15,7 @@ api.include_router(recipes.router)
 api.include_router(guides.router)
 api.include_router(policies.router)
 api.include_router(lineage.router)
+
+if __name__ == "__main__":
+    # Run Uvicorn programmatically
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
